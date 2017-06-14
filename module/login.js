0f3e048a5e8456f7fbe5cadedc08dec531621c3e
yuanjing <yuanjing@egret.com>
2017-06-14 15:54:39 +0800

    aa

--- module/login.js
+++ module/login.js
@@ -8,7 +8,7 @@ Login.userId=function(funData,callBack,callTar)
     console.log("3----",funData);
     if(funData.p.name == this.name)
     {
-        var rd = { "name":"你是","age" : "12","address" : "1231","code":0};
+        var rd = { "name":"你是","age" : "12","address" : "1231","code":   0};
     }else{
         var rd = { "err":"用户名不对"};
     }
